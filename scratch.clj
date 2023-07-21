79bf9a3564c64b08ba0c18ebe2ffb030b775ef9f
Bobbi Towers <btowers793@gmail.com>
2023-07-21 16:54:12 -0700

    add exercise test suites as JSON

--- scratch.clj
+++ scratch.clj
@@ -12,6 +12,22 @@
           f (fs/file "exercises\\practice" slug "src" (str filename ".clj"))]
       (slurp f))))
 
+(def exercises
+  (map #(subs (str/replace % ".clj" "") 15)
+       (fs/list-dir "exercise_tests")))
+
+(def test-all
+  (for [e exercises]
+    (let [f (fs/file "exercise_tests" (str e ".clj"))]
+      (slurp f))))
+
 (comment
-  (spit "exercises.json" (json/generate-string (zipmap practice-exercises src-all) {:pretty true}))
+  (spit "exercises.json" 
+        (json/generate-string
+         (zipmap practice-exercises src-all) 
+         {:pretty true}))
+  (spit "tests.json"
+        (json/generate-string
+         (zipmap exercises test-all)
+         {:pretty true}))
   )
